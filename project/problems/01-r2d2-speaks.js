c5bd25157822a22659383860eaaf5a84a0e6386b
Kevin Mei <kevjinx@gmail.com>
2021-04-24 16:11:06 -0700

    finish1

--- project/problems/01-r2d2-speaks.js
+++ project/problems/01-r2d2-speaks.js
@@ -1,7 +1,7 @@
 /*
     Write a function r2d2Speaks that takes in a series of 0s and 1s.
     The function should console.log a 'beep' for a 0 followed by a pause of 400 ms and a 'boop' for a 1 followed by 800 ms.
-    
+
     let code = [0, 1, 1, 0];
     r2d2Speaks(code);
         print 'beep'
@@ -16,15 +16,50 @@
     There are no mocha tests for this code.
     Run the following two lines beneath the function
     to see if your code demonstrates the expected behavior.
-   
+
 */
 
 function r2d2Speaks(code) {
-    
+  let delay = 0;
+  code.forEach(reee => {
+    if(!reee) {
+      setTimeout(() => {
+        console.log('beep');
+      }, delay);
+      delay += 400;
+    } else {
+      setTimeout(() => {
+        console.log('boop');
+      }, delay);
+      delay += 800;
+    }
+  })
+}
+
+const r2d2SpeaksRecursive = (code, delay = 0) => {
+  if (!code.length) {
+    return;
+  } else {
+    if (!code[0]) {
+        console.log('beep');
+        delay = 400;
+    } else {
+        console.log('boop');
+        delay = 1000;
+    }
+
+    code.shift();
+
+    setTimeout(() => {
+      r2d2SpeaksRecursive(code)
+    }, delay);
+
+  }
 }
 
 let code = [0, 1, 1, 0];
-r2d2Speaks(code);
+// r2d2Speaks(code);
+// r2d2SpeaksRecursive(code)
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
@@ -32,4 +67,3 @@ try {
 } catch (e) {
     module.exports = null;
 }
-
